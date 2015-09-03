59c4dd3e6a9c7d0cfb660bd7b89d6ba9cf6c33e7
baudensc <baudensc@frene12.iut-infobio.priv.univ-lille1.fr>
2015-09-03 14:32:57 +0200

    ajout des tests HAND

--- BlackJack/src/tests/HandTest.java
+++ BlackJack/src/tests/HandTest.java
@@ -1,12 +1,17 @@
 package tests;
 
+import static org.junit.Assert.*;
 import static org.junit.Assert.assertTrue;
+
+import java.util.List;
 
 import org.junit.Test;
 
 import com.blackjack.Carte;
+import com.blackjack.Couleur;
 import com.blackjack.Hand;
 import com.blackjack.Pioche;
+import com.blackjack.Valeur;
 
 public class HandTest {
 	@Test
@@ -22,13 +27,24 @@ public class HandTest {
 	@Test
 	public void retirerCarte_doit_retirer_carte_main_joueur(){
 		Hand main = new Hand();
-		Carte carte = new Carte(null, null);
 		
-		main.ajouterCarte(carte);
-		main.ajouterCarte(carte);
-		assertTrue(main.getSize() == 2);
-		main.retirerCarte(carte);
+		Carte carte1 = new Carte(Couleur.COEUR,Valeur.CINQ);
+		main.ajouterCarte(carte1);
+		
 		assertTrue(main.getSize() == 1);
+		main.retirerCarte(carte1);
+		assertTrue(main.getSize() == 0);
+	}
+	
+	@Test
+	public void getValeur_doit_retourner_les_valeurs_des_cartes_de_la_main(){
+		Hand main = new Hand();
+		main.ajouterCarte(new Carte(Couleur.CARREAU,Valeur.NEUF));
+		main.ajouterCarte(new Carte(Couleur.CARREAU,Valeur.SIX));
+		main.ajouterCarte(new Carte(Couleur.PIQUE,Valeur.DIX));
+		List<Valeur> lesValeurs = main.getValeurs();
+		assertEquals(3, lesValeurs.size());
+		assertTrue(lesValeurs.contains(Valeur.NEUF));
 	}
 
 }

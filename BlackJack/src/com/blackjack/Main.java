6c1455da7c163b489205bbcde60326c288565c24
messiaek <messiaek@frene10.iut-infobio.priv.univ-lille1.fr>
2015-09-03 14:56:07 +0200

    rejouer

--- BlackJack/src/com/blackjack/Main.java
+++ BlackJack/src/com/blackjack/Main.java
@@ -12,37 +12,39 @@ public class Main {
 	public static void main(String[] args) {
 		
 
-		int nbrJoueurs = 0;
-		
 		do {
+			int nbrJoueurs = 0;
 			
-			String nJ = JOptionPane.showInputDialog(null, "Entrez le nombre de joueurs\n(entre 1 à 4)");
+			do {
+				
+				String nJ = JOptionPane.showInputDialog(null, "Entrez le nombre de joueurs\n(entre 1 à 4)");
+				
+				try {
+					
+					int entry = Integer.parseInt(nJ);
+					
+					if (entry > 0 && entry < 5)
+						nbrJoueurs = entry;
+					
+				} catch (Exception ex) {
+					if (nJ == null)
+						System.exit(0);
+				}
+				
+				
+			} while (nbrJoueurs == 0);
 			
-			try {
-				
-				int entry = Integer.parseInt(nJ);
-				
-				if (entry > 0 && entry < 5)
-					nbrJoueurs = entry;
-				
-			} catch (Exception ex) {
-				if (nJ == null)
+			List<Joueur> joueurs = new ArrayList<Joueur>();
+
+			for (int i = 0; i < nbrJoueurs; i ++) {
+				String nomJ = JOptionPane.showInputDialog(null, "Entrez le nom du joueur " + (i + 1));
+				joueurs.add(new Joueur(nomJ));
+				if (nomJ == null)
 					System.exit(0);
 			}
 			
-			
-		} while (nbrJoueurs == 0);
-		
-		List<Joueur> joueurs = new ArrayList<Joueur>();
-		
-		for (int i = 0; i < nbrJoueurs; i ++) {
-			String nomJ = JOptionPane.showInputDialog(null, "Entrez le nom du joueur " + (i + 1));
-			joueurs.add(new Joueur(nomJ));
-			if (nomJ == null)
-				System.exit(0);
-		}
-		
-		Jeu jeu = new Jeu(joueurs);
+			Jeu jeu = new Jeu(joueurs);
+		} while (JOptionPane.showConfirmDialog(null, "voulez-vous rejouer?") == 0);
 		
 	}
 

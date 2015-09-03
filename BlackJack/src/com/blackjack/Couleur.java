343ce4c69e7d085f765569381c499d7e1e4cd7ce
Adrien <adrien.philippaux@gmail.com>
2015-09-03 13:47:06 +0200

    Javadoc complete sauf jeu

--- BlackJack/src/com/blackjack/Couleur.java
+++ BlackJack/src/com/blackjack/Couleur.java
@@ -5,9 +5,15 @@ import java.awt.Color;
 public enum Couleur {
 
 	CARREAU(Color.red), COEUR(Color.red), PIQUE(Color.black), TREFLE(Color.black);
-	
+	/**
+	 * Couleurs des signes
+	 */
 	public Color color;
 	
+	/**
+	 * Crée l'énumération des couleurs
+	 * @param color - couleur du signe
+	 */
 	private Couleur(Color color ) {
 		this.color = color;
 	}

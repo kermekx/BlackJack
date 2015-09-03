627b9ae44561b6353bc4c6d29537e1c052ef629f
messiaek <messiaek@frene10.iut-infobio.priv.univ-lille1.fr>
2015-09-03 09:05:11 +0200

    Changement de "damme" en "dame".

--- BlackJack/src/com/blackjack/Valeur.java
+++ BlackJack/src/com/blackjack/Valeur.java
@@ -5,7 +5,7 @@ import java.util.List;
 public enum Valeur {
 
 	AS(1), DEUX(2), TROIS(3), QUATRE(4), CINQ(5), SIX(6), SEPT(7), HUIT(8), NEUF(
-			9), DIX(10), VALET(10), DAMME(10), ROI(10);
+			9), DIX(10), VALET(10), DAME(10), ROI(10);
 
 	private int points;
 

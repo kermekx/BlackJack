48a4cb8f45e81fdb1b639974a4646b9c606610cf
Adrien <adrien.philippaux@gmail.com>
2015-09-03 08:25:59 +0200

    Mess coucou

--- BlackJack/src/com/blackjack/Main.java
+++ BlackJack/src/com/blackjack/Main.java
@@ -3,9 +3,10 @@ package com.blackjack;
 public class Main {
 	
 	public static void main(String[] args) {
+
 		System.out.println("Hello World");
 		System.out.println("salut");
-		System.out.println("test");
+		System.out.println("Coucou");
 	}
 
 }

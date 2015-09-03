66d6a7948ef4a6093aad1aab46152c1486d7efc8
Adrien <adrien.philippaux@gmail.com>
2015-09-03 11:21:17 +0200

    JAVADOC

--- BlackJack/src/com/blackjack/Carte.java
+++ BlackJack/src/com/blackjack/Carte.java
@@ -4,20 +4,39 @@ public class Carte {
 	
 	Couleur couleur;
 	Valeur valeur;
+	
+	/**
+	 * Constructeur d'une carte
+	 * @param couleur - Couleur de la carte : Rouge (Carrreau, coeur) ou Noire (Pique,Trèfle)
+	 * @param valeur - La valeur de la carte de AS à ROI
+	 */
 	public Carte (Couleur couleur, Valeur valeur){
 		this.couleur = couleur;
 		this.valeur=valeur;
 	}
 	
+	/**
+	 * Permet d'obtenir la couleur de la carte
+	 * @return	- la couleur de la carte
+	 */
 	public Couleur getCouleur(){
 		return couleur;
 	}
 	
+	/**
+	 * Retourne la valeur de la carte
+	 * @return - la valeur de la carte
+	 */
 	public Valeur getValeur(){
 		return valeur;
 		
 	}
 	
+	/**
+	 * Méthode permettant de savoir si 2 cartes sont égales, sont les mêmes
+	 * @param carte à comparer 
+	 * @return true ou false selon l'équivalence
+	 */
 public boolean equals(Carte carte){
 	if(carte.couleur.equals(this.couleur) && carte.valeur.equals(this.valeur)){
 		return true;
@@ -27,6 +46,9 @@ public boolean equals(Carte carte){
 	
 }
 	@Override
+	/**
+	 * Retourne les informations de la carte : la valeur et sa couleur
+	 */
 	public String toString() {
 		return "Carte [couleur=" + couleur + ", valeur=" + valeur + "]";
 	}
--- BlackJack/src/com/blackjack/Hand.java
+++ BlackJack/src/com/blackjack/Hand.java
@@ -6,48 +6,63 @@ import java.util.List;
 public class Hand {
 
 	List<Carte> cartes;
-	int pts;
-
+	
+	/**
+	 * Construit une main qui sera constituée de cartes
+	 */
 	public Hand() {
 		cartes = new ArrayList<Carte>();
 	}
-
+	
+	/**
+	 * Retourne la liste des cartes de la main
+	 * @return la liste des cartes présentes dans la main
+	 */
 	public List<Carte> getCartes() {
 		return cartes;
 	}
-
+	/**
+	 * Pour modifier la liste des cartes
+	 * @param cartes - liste à échanger 
+	 */
 	public void setCartes(List<Carte> cartes) {
 		this.cartes = cartes;
 	}
 
-	public int getPts() {
-		return pts;
-	}
-
-	public void setPts(int pts) {
-		this.pts = pts;
-	}
-
-	public void ajoutCarte(Pioche carte) {
-		cartes.add(carte.piocherCarte());
-	}
-
+	/**
+	 * retire la carte du paquet
+	 * @param carte - la carte à retirer du paquet 
+	 */
 	public void retirerCarte(Carte carte) {
 		cartes.remove(carte);
 	}
+	/**
+	 * Retourne la chaine de caractères permettant de représenter les cartes
+	 */
 	public String toString(){
 		return cartes.toString();
 	}
 
+	/**
+	 * Ajoute la carte à la main
+	 * @param carte - carte à ajouter à la main
+	 */
 	public void ajouterCarte(Carte carte) {
 		cartes.add(carte);
 		
 	}
+	/**
+	 * Permet d'obtenir le nombre de cartes composant la main 
+	 * @return - un entier correspondant au nombre de cartes
+	 */
 	public int getSize(){
 		return cartes.size();
 	}
 	
-
+/**
+ * Permet d'obtenir toutes les valeurs des cartes de la main
+ * @return - les valeurs des cartes présentes dans la main 
+ */
 	public List<Valeur> getValeurs(){
 		List<Valeur> val = new ArrayList<Valeur>();
 		for(Carte carte : cartes)

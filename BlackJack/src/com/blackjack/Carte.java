b3f23ebb664384a4cc12f7f441ae1ea9c218f65b
Corwin Fevre <corwin.fevre@online.fr>
2015-09-03 08:58:04 +0200

    add Carte.java

---
+++ BlackJack/src/com/blackjack/Carte.java
@@ -0,0 +1,21 @@
+package com.blackjack;
+
+public class Carte {
+	
+	Couleur couleur;
+	Valeur valeur;
+	public Carte (Couleur couleur, Valeur valeur){
+		this.couleur = couleur;
+		this.valeur=valeur;
+	}
+	
+	public Couleur getCouleur(){
+		return couleur;
+	}
+	
+	public Valeur getValeur(){
+		return valeur;
+	}
+	
+	
+}

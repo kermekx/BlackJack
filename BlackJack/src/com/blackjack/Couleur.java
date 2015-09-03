65315c8fa2b980a620a2430c4a7e101bf5ddc7b7
Corwin Fevre <corwin.fevre@online.fr>
2015-09-03 09:12:45 +0200

    Update Couleur.java (recup couleur de la carte)

--- BlackJack/src/com/blackjack/Couleur.java
+++ BlackJack/src/com/blackjack/Couleur.java
@@ -1,11 +1,17 @@
 package com.blackjack;
 
+import java.awt.Color;
+
 public enum Couleur {
 
-	CARREAU,COEUR,PIQUE,TREFLE;
-
-	public getCouleur(){
-		
+	CARREAU(Color.red), COEUR(Color.red), PIQUE(Color.black), TREFLE(Color.black);
+	
+	public Color color;
+	
+	private Couleur(Color color ) {
+		this.color = color;
 	}
+	
+	
 }
 

f597add74a959dce6a33b509c27a64c870dbae00
messiaek <messiaek@frene10.iut-infobio.priv.univ-lille1.fr>
2015-09-03 11:19:29 +0200

    Ajout de damande de pioche

--- BlackJack/src/com/blackjack/Jeu.java
+++ BlackJack/src/com/blackjack/Jeu.java
@@ -1,11 +1,15 @@
 package com.blackjack;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import javax.swing.JOptionPane;
 
 public class Jeu {
 	
 	private List<Joueur> joueurs;
+	private Map<Joueur, Boolean> fini = new HashMap<Joueur, Boolean>();
 	
 	private Pioche pioche;
 	
@@ -20,7 +24,10 @@ public class Jeu {
 		for(Joueur joueur : joueurs) {
 			System.out.println(joueur.toString());
 		}
-			
+		
+		while (!fini()) {
+			tour();
+		}
 		
 	}
 	
@@ -29,7 +36,30 @@ public class Jeu {
 		for (Joueur joueur : joueurs) {
 			joueur.prendreCarte(pioche.piocherCarte());
 			joueur.prendreCarte(pioche.piocherCarte());
+			fini.put(joueur, false);
 		}
+		
+	}
+	
+	public boolean fini() {
+		
+		boolean fin = true;
+		
+		for (Joueur joueur : joueurs)
+			if (fini.get(joueur) == false)
+				fin = false;
+		
+		return fin;
+		
+	}
+	
+	public void tour() {
+		for (Joueur joueur : joueurs)
+			if (fini.get(joueur) == false) {
+				JOptionPane.showConfirmDialog(null, joueur.toString() + "\n  voulez vous piochez?");
+				
+			}
+				
 		
 	}
 

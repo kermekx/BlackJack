07aff4617dbd2a066192c787204cf1b1ee159de9
messiaek <messiaek@frene10.iut-infobio.priv.univ-lille1.fr>
2015-09-03 11:04:16 +0200

    Selection des joueurs

--- BlackJack/src/com/blackjack/Main.java
+++ BlackJack/src/com/blackjack/Main.java
@@ -3,25 +3,39 @@ package com.blackjack;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.swing.JOptionPane;
+
 
 public class Main {
 	
 
 	public static void main(String[] args) {
 		
-		/**
+
 		int nbrJoueurs = 0;
 		
 		do {
 			
+			String nJ = JOptionPane.showInputDialog(null, "Entrez le nombre de joueurs\n(entre 1 à 4)");
+			
+			try {
+				
+				int entry = Integer.parseInt(nJ);
+				
+				if (entry > 0 && entry < 5)
+					nbrJoueurs = entry;
+				
+			} catch (Exception ex) {
+				System.out.println(nJ + " n'est pas reconnue");
+			}
+			
 			
 		} while (nbrJoueurs == 0);
-		*/
+		
 		List<Joueur> joueurs = new ArrayList<Joueur>();
 		
-		joueurs.add(new Joueur("Corwin"));
-		
-		joueurs.add(new Joueur("Kevin"));
+		for (int i = 0; i < nbrJoueurs; i ++)
+			joueurs.add(new Joueur(JOptionPane.showInputDialog(null, "Entrez le nom du joueur " + (i + 1))));
 		
 		Jeu jeu = new Jeu(joueurs);
 		

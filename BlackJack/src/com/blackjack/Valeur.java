7b3423be245687db89452d5a1f8151cb58cb3a26
messiaek <messiaek@frene10.iut-infobio.priv.univ-lille1.fr>
2015-09-03 08:57:19 +0200

    class valeur

---
+++ BlackJack/src/com/blackjack/Valeur.java
@@ -0,0 +1,40 @@
+package com.blackjack;
+
+import java.util.List;
+
+public enum Valeur {
+
+	AS(1), DEUX(2), TROIS(3), QUATRE(4), CINQ(5), SIX(6), SEPT(7), HUIT(8), NEUF(
+			9), DIX(10), VALET(10), DAMME(10), ROI(10);
+
+	private int points;
+
+	private Valeur(int points) {
+
+		this.points = points;
+
+	}
+
+	public static int getPoints(List<Valeur> valeurs) {
+
+		int nbrAs = 0;
+		int points = 0;
+
+		for (Valeur valeur : valeurs) {
+			points += valeur.points;
+			if (valeur == AS)
+				nbrAs ++;
+		}
+		
+		while (nbrAs > 0) {
+			if (points <= 11)
+				points += 10;
+			
+			nbrAs --;
+		}
+
+		return points;
+
+	}
+
+}
